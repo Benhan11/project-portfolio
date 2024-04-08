93f3a1ec78a19c1961bfaec42e5e62989476a644
Benjamin Hansson <benjamin.hansson@gmail.com>
2024-04-08 16:09:26 +0200

    Made snippet images of different aspect ratios render to the same maximum height and centered them

--- src/components/ProjectSnippet.tsx
+++ src/components/ProjectSnippet.tsx
@@ -22,9 +22,13 @@ type SnippetType = {
 function ProjectSnippet({ snippet }: SnippetProps) {
     return (
         <div className="flex flex-row bg-green-500/15 shadow-xl gap-x-10 px-6 py-4 mb-12 border border-slate-400 rounded-md">
-            <div className="flex-none w-32">
-                {snippet.imageName !== '' && <img src={images[snippet.imageName]} className="mt-1 h-auto max-w-full rounded" />}
-            </div>
+            {snippet.imageName !== '' && 
+                <div className="flex-none w-32">
+                    <div className="flex flex-row justify-center">
+                        <img src={images[snippet.imageName]} className="mt-1 h-auto max-w-full max-h-32 rounded" />
+                    </div>
+                </div>
+            }
             <div className="space-y-2">
                 <h2 className="text-slate-100">{snippet.title}</h2>
                 <p>{snippet.description}</p>
