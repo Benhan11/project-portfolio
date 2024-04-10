416eb96e53e0a6052ecb2d104fc1615fe6b7d392
Benjamin Hansson <benjamin.hansson@gmail.com>
2024-04-10 17:59:43 +0200

    Created a dynamic icon export file, following the same pattern as for images

---
+++ src/assets/images/icons.ts
@@ -0,0 +1,12 @@
+import { 
+    faJs as jsIcon, 
+    faNodeJs as nodeJsIcon, 
+    faBootstrap as bootstrapIcon
+} from '@fortawesome/free-brands-svg-icons';
+
+
+export default {
+    jsIcon,
+    nodeJsIcon,
+    bootstrapIcon,
+}
