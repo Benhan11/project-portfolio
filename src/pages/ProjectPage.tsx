3428ab55b8728ae4c627cb072f78dc0e6690e36a
Benjamin Hansson <benjamin.hansson@gmail.com>
2024-04-10 12:39:47 +0200

    Prototype styling for technology tags on the project page

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -8,12 +8,16 @@ function ProjectPage() {
             <div className="border mx-auto max-w-screen-xl px-6 md:px-12 lg:px-24">
                 <div className="border lg:py-24 md:py-12 sm:py-6">
                     <h1 className="text-slate-100 text-3xl font-medium">Youtube playlist saver</h1>
-                    <h2 className="text-lg">
-                        <FontAwesomeIcon icon={faJs} />JavaScript
-                        <FontAwesomeIcon icon={faNodeJs} />Node.js
-                        <FontAwesomeIcon icon={faBootstrap} />Bootstrap
-                        <FontAwesomeIcon icon={faJs} />EJS
-                    </h2>
+                    <div className="flex flex-wrap max-w-80 pt-1 gap-y-1">
+                        <span className="flex-none border rounded-full border-yellow-500 bg-yellow-200/5 text-yellow-500 px-2 py-0.5 me-1">
+                            <FontAwesomeIcon icon={faJs} /> JavaScript</span>
+                        <span className="flex-none border rounded-full border-lime-500 bg-lime-200/5 text-lime-500 px-2 py-0.5 me-1">
+                            <FontAwesomeIcon icon={faNodeJs} /> Node.js</span>
+                        <span className="flex-none border rounded-full border-sky-500 bg-sky-200/5 text-sky-500 px-2 py-0.5 me-1">
+                            <FontAwesomeIcon icon={faBootstrap} /> Bootstrap</span>
+                        <span className="flex-none border rounded-full border-red-500 bg-red-200/5 text-red-500 px-2 py-0.5 me-1">
+                            <FontAwesomeIcon icon={faJs} /> EJS</span>
+                    </div>
                 </div>
                 <div className="border lg:flex lg:justify-between">
                     <div className="border lg:w-1/2 lg:py-24 md:py-12 sm:py-6">
