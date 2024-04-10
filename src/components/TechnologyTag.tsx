a14f2f78971a0477a53d1868a4d6e414be2d5ed7
Benjamin Hansson <benjamin.hansson@gmail.com>
2024-04-10 17:58:39 +0200

    Created a component from the prototype technology tags

---
+++ src/components/TechnologyTag.tsx
@@ -0,0 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import iconsRaw from 'assets/images/icons';
+
+
+type IconsType = {
+    [key: string]: IconProp;
+}
+
+const icons: IconsType = iconsRaw;
+
+
+interface TagProps {
+    data: TagType;
+}
+
+type TagType = {
+    name: string;
+    color: string;
+    iconName: string;
+}
+
+
+function TechnologyTag({ data }: TagProps) {
+    return (
+        <span className={"flex-none border rounded-full border-" + data.color + "-500 bg-" + data.color + "-200/5 text-" + data.color + "-500 px-2 py-0.5 me-1"}>
+            <FontAwesomeIcon icon={icons[data.iconName]} /> {data.name}</span>
+    );
+}
+
+export default TechnologyTag;
