6fcc2ecc6af49f2e2ba382af05c51ebe89c80fca
Benjamin Hansson <benjamin.hansson@gmail.com>
2024-04-10 18:07:40 +0200

    Project page now renders an associated image

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,14 +1,23 @@
 import projectData from 'assets/project_page_data.json';
 import TechnologyTag from 'components/TechnologyTag';
 
+import imagesRaw from 'assets/images/images';
+
+
+type ImagesType = {
+    [key: string]: string;
+}
+
+const images: ImagesType = imagesRaw;
+
 
 function ProjectPage() {
     let tempDataProp = projectData.content[0];
 
     return (
         <div className="min-h-screen font-mono text-slate-400 bg-gradient-to-r from-black via-violet-950 to-black">
-            <div className="border mx-auto max-w-screen-xl px-6 md:px-12 lg:px-24">
-                <div className="border lg:py-24 md:py-12 sm:py-6">
+            <div className="mx-auto max-w-screen-xl px-6 md:px-12 lg:px-24">
+                <div className="lg:py-24 md:py-12 sm:py-6">
                     <h1 className="text-slate-100 text-3xl font-medium">Youtube playlist saver</h1>
                     <div className="flex flex-wrap max-w-80 pt-1 gap-y-1">
                         {tempDataProp.tags.map(tag => 
@@ -17,15 +26,15 @@ function ProjectPage() {
                     </div>
                 </div>
                 {tempDataProp.dataSections.map(section => 
-                    <div className="border lg:flex lg:justify-between">
-                        <div className="border lg:w-1/2 lg:py-24 md:py-12 sm:py-6">
+                    <div className="lg:flex lg:justify-between">
+                        <div className="lg:w-1/2 lg:py-24 md:py-12 sm:py-6">
                             <div className="">
                                 <a className="text-lg font-bold bg-gradient-to-r from-pink-500 to-yellow-500 inline-block text-transparent bg-clip-text">{section.title}</a><br/>
                                 {section.text}
                             </div>
                         </div>
-                        <div className="lg:w-1/2 lg:py-24">
-                            IMAGE
+                        <div className="flex lg:w-1/2 justify-end">
+                            <img src={images[section.imageName]} className="shadow-lg max-w-full max-h-96 rounded" />
                         </div>
                     </div>
                 )}
