4c97608bb2bf4a4d52d81db7fc28ab57873758c9
Benjamin Hansson <benjamin.hansson@gmail.com>
2024-04-10 11:45:38 +0200

    Project snippets now hoverable with nice transitions

--- src/components/ProjectSnippet.tsx
+++ src/components/ProjectSnippet.tsx
@@ -21,7 +21,8 @@ type SnippetType = {
 
 function ProjectSnippet({ snippet }: SnippetProps) {
     return (
-        <div className="flex flex-row bg-green-500/15 shadow-xl gap-x-10 px-6 py-4 mb-12 border border-slate-400 rounded-md">
+        <div className="flex flex-row bg-green-500/15 shadow-xl gap-x-10 px-6 py-4 mb-12 border border-slate-400 rounded-md 
+                        transition ease-in-out delay-0 duration-200 hover:translate-y-1 hover:scale-105 hover:bg-green-500/20 hover:shadow-2xl">
             {snippet.imageName !== '' && 
                 <div className="flex-none w-32">
                     <div className="flex flex-row justify-center">
