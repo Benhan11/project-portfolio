0d5847f9269499137db50078be1dcb90806a6ab8
Benjamin Hansson <benjamin.hansson@gmail.com>
2024-04-11 15:10:10 +0200

    Created a workaround for dynamic class assignment to work for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,10 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    { pattern: /border-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-500/ },
+    { pattern: /bg-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(200\/5)/ },
+    { pattern: /text-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-500/ },
+  ]
 }
 
