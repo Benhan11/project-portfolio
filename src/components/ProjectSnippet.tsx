e5d5dec56bcd58d476d47173bfec1a66a442923d
Benjamin Hansson <benjamin.hansson@gmail.com>
2024-05-10 15:55:03 +0200

    Homepage responsiveness

--- src/components/ProjectSnippet.tsx
+++ src/components/ProjectSnippet.tsx
@@ -18,8 +18,8 @@ type SnippetType = {
 
 function ProjectSnippet({ snippet }: SnippetProps) {
     return (
-        <Link to={`/project/${snippet.projectId}`} className="flex flex-row bg-green-500/15 shadow-xl gap-x-10 px-6 py-4 mb-12 border border-slate-400 rounded-md 
-                        transition ease-in-out delay-0 duration-200 hover:translate-y-1 hover:scale-105 hover:bg-green-500/20 hover:shadow-2xl">
+        <Link to={`/project/${snippet.projectId}`} className="flex sm:flex-row flex-row-reverse sm:bg-green-500/15 shadow-xl gap-x-10 sm:px-6 py-4 mb-12 sm:border border-b border-slate-400 sm:rounded-md rounded 
+                        transition ease-in-out delay-0 duration-200 hover:translate-y-1 sm:hover:scale-105 sm:hover:bg-green-500/20 hover:shadow-2xl">
             {snippet.imageName !== '' && 
                 <div className="flex-none w-32">
                     <div className="flex flex-row justify-center">
@@ -28,7 +28,7 @@ function ProjectSnippet({ snippet }: SnippetProps) {
                 </div>
             }
             <div className="space-y-2">
-                <div className="flex justify-between">
+                <div className="flex sm:justify-between space-x-3">
                     <h2 className="text-slate-100">{snippet.title}</h2>
                     <IconWrapper icon={icons['opensNewPageBoxIcon']} size="sm" className="mt-1" />
                 </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,10 +10,10 @@ function HomePage() {
     return (
         <div className="min-h-screen font-mono text-slate-400 bg-gradient-to-r from-black via-violet-950 to-black">
             <div className="mx-auto max-w-screen-xl px-6 md:px-12 lg:px-24">
-                <div className="lg:flex lg:justify-between">
-                    <div className="lg:flex lg:flex-col lg:sticky lg:top-0 lg:max-h-screen lg:w-1/2 lg:py-24 md:py-12 sm:py-6">
+                <div className="lg:flex lg:justify-between lg:space-y-0 space-y-4">
+                    <div className="lg:flex lg:flex-col lg:sticky lg:top-0 lg:max-h-screen lg:w-1/2 lg:py-24 md:py-12 pt-6 pb-12">
                         <div>
-                            <a href="https://github.com/Benhan11/portfolio-app" target="_blank" rel="noopener noreferrer" className="flex flex-row text-slate-100 text-3xl font-medium space-x-1">
+                            <a href="https://github.com/Benhan11/project-portfolio" target="_blank" rel="noopener noreferrer" className="flex flex-row text-slate-100 text-3xl font-medium space-x-1">
                                 <div>Project portfolio</div>
                                 <IconWrapper icon={icons['opensNewPageArrowIcon']} transform="shrink-8 rotate-45 up-2" />
                             </a>
