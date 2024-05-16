326038387e76d781f8dc0e75f550ad01af372c35
Benjamin Hansson <benjamin.hansson@gmail.com>
2024-05-16 11:47:01 +0200

    Made images more visible at different screen widths

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -19,7 +19,7 @@ function ProjectPage() {
 
     return (
         <div className="min-h-screen font-mono text-slate-400 bg-gradient-to-r from-black via-violet-950 to-black">
-            <div className="mx-auto max-w-screen-xl lg:px-24 md:px-12 px-6">
+            <div className="mx-auto max-w-screen-xl lg:px-24 md:px-12 sm:px-24 px-6">
                 <div className="lg:py-20 md:py-12 pt-6 pb-16">
                     <div className="sm:mb-3 mb-8">
                         <Link to="/"><IconWrapper icon={icons['backIcon']} size="sm" /> Home</Link>
@@ -35,15 +35,15 @@ function ProjectPage() {
                 </div>
                 {project.dataSections.map((section, index) => 
                     <div key={index} ref={sectionsRef.current[index]} className={`md:flex md:justify-between md:space-x-12 pb-36 sm:transition-opacity sm:ease-in sm:duration-200 ${isVisibleArray[index] ? "sm:opacity-100" : "sm:opacity-30"}`}>
-                        <div className="lg:w-1/2 md:w-2/3 flex items-center">
+                        <div className="md:w-1/2 flex items-center">
                             <div>
                                 <a className="text-lg font-bold bg-gradient-to-r from-pink-500 to-yellow-500 inline-block text-transparent bg-clip-text sm:pb-0 pb-2">{section.title}</a><br/>
                                 {section.text}
                             </div>
                         </div>
-                        <div className="flex flex-row lg:w-1/2 md:w-1/3 w-full pt-10">
+                        <div className="flex flex-row md:w-1/2 w-full pt-10">
                             <div className="flex w-full md:justify-end justify-center items-center">
-                                <img src={images[section.imageName]} className="max-w-full md:max-h-96 sm:max-h-64 max-h-48 border-2 border-slate-600 rounded shadow-2xl" />
+                                <img src={images[section.imageName]} className="max-w-full max-h-96 border-2 border-slate-600 rounded shadow-2xl" />
                             </div>
                         </div>
                     </div>
