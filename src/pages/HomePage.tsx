c9ae8a13a16e4f27d5d99ead5dc56efcaf5f4a21
Benjamin Hansson <benjamin.hansson@gmail.com>
2024-05-26 11:50:13 +0200

    Github and Linkedin hover colors

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -19,8 +19,8 @@ function HomePage() {
                             </a>
                             <h2 className="text-lg">Benjamin Hansson</h2>
                             <div className="space-x-2 pt-2">
-                                <a href="https://www.linkedin.com/in/benjamin-hansson-88131b1a2/" target="_blank" rel="noopener noreferrer"><IconWrapper icon={icons['linkedinIcon']} size="xl" /></a>
-                                <a href="https://github.com/Benhan11" target="_blank" rel="noopener noreferrer"><IconWrapper icon={icons['githubIcon']} size="lg" /></a>
+                                <a href="https://www.linkedin.com/in/benjamin-hansson-88131b1a2/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500"><IconWrapper icon={icons['linkedinIcon']} size="xl" /></a>
+                                <a href="https://github.com/Benhan11" target="_blank" rel="noopener noreferrer" className="hover:text-white"><IconWrapper icon={icons['githubIcon']} size="lg" /></a>
                             </div>
                             <p className="max-w-72 pt-16">
                                 <a className="text-lg font-bold bg-gradient-to-r from-pink-500 to-yellow-500 inline-block text-transparent bg-clip-text">Welcome!</a><br/>
