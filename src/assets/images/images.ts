aba220de36220c84cae9632015be02cbd41ca693
Benjamin Hansson <benjamin.hansson@gmail.com>
2024-06-07 14:36:20 +0200

    Populated app with project data

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -1,7 +1,34 @@
-import portfolioSnippetImage from 'assets/images/project-images/project-portfolio/snippet.png'
-import youtubeSnippetImage from 'assets/images/project-images/youtube-playlist-saver/snippet.png';
-import spotifySnippetImage from 'assets/images/project-images/spotify-playlist-saver/snippet.png';
-import hueSnippetImage from 'assets/images/project-images/hue-lamp-app/snippet.png';
+import portfolioSnippet from 'assets/images/project-images/project-portfolio/snippet.png'
+import portfolioStack from 'assets/images/project-images/project-portfolio/stack.png'
+import portfolioDynamic from 'assets/images/project-images/project-portfolio/dynamic.svg'
+import portfolioFile from 'assets/images/project-images/project-portfolio/file.svg'
+import portfolioImages from 'assets/images/project-images/project-portfolio/images.svg'
+
+import portfolioDocumentation from 'assets/images/project-images/project-portfolio/documentation.svg'
+
+import youtubeSnippet from 'assets/images/project-images/youtube-playlist-saver/snippet.png';
+import youtubeStack from 'assets/images/project-images/youtube-playlist-saver/stack.png';
+import youtubeOld from 'assets/images/project-images/youtube-playlist-saver/old.png';
+import youtubeAuth from 'assets/images/project-images/youtube-playlist-saver/auth_page.png';
+import youtubeApi from 'assets/images/project-images/youtube-playlist-saver/api.svg';
+import youtubeCallback from 'assets/images/project-images/youtube-playlist-saver/callback.svg';
+import youtubeDocumentation from 'assets/images/project-images/youtube-playlist-saver/documentation.svg';
+
+import spotifySnippet from 'assets/images/project-images/spotify-playlist-saver/snippet.png';
+import spotifyApp from 'assets/images/project-images/spotify-playlist-saver/app.png';
+import spotifyStack from 'assets/images/project-images/spotify-playlist-saver/stack.svg';
+import spotifyOld from 'assets/images/project-images/spotify-playlist-saver/old.png';
+import spotifyAuth from 'assets/images/project-images/spotify-playlist-saver/auth.svg';
+import spotifyApi from 'assets/images/project-images/spotify-playlist-saver/api.svg';
+import spotifyDocumentation from 'assets/images/project-images/spotify-playlist-saver/documentation.svg';
+
+import hueSnippet from 'assets/images/project-images/hue-lamp-app/snippet.png';
+import hueLights from 'assets/images/project-images/hue-lamp-app/lights.png';
+import hueEvents from 'assets/images/project-images/hue-lamp-app/events.svg';
+import hueApi from 'assets/images/project-images/hue-lamp-app/api.svg';
+import hueColorTheory from 'assets/images/project-images/hue-lamp-app/color_theory.svg';
+import hueUtilities from 'assets/images/project-images/hue-lamp-app/utilities.svg';
+import hueDocumentation from 'assets/images/project-images/hue-lamp-app/documentation.svg';
 
 
 type ImagesType = {
@@ -9,10 +36,37 @@ type ImagesType = {
 }
 
 const images: ImagesType = {
-    portfolioSnippetImage,
-    youtubeSnippetImage,
-    spotifySnippetImage,
-    hueSnippetImage
+    portfolioSnippet,
+    portfolioStack,
+    portfolioDynamic,
+    portfolioFile,
+    portfolioImages,
+
+    portfolioDocumentation,
+
+    youtubeSnippet,
+    youtubeStack,
+    youtubeOld,
+    youtubeAuth,
+    youtubeApi,
+    youtubeCallback,
+    youtubeDocumentation,
+
+    spotifySnippet,
+    spotifyApp,
+    spotifyStack,
+    spotifyOld,
+    spotifyAuth,
+    spotifyApi,
+    spotifyDocumentation,
+
+    hueSnippet,
+    hueLights,
+    hueEvents,
+    hueApi,
+    hueColorTheory,
+    hueUtilities,
+    hueDocumentation,
 };
 
 
